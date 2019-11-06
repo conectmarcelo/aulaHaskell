e32557bd29dab473084d64f4203bccf8bd4f0692
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-06 04:13:16 +0000

    teste

--- src/Handler/Common.hs
+++ src/Handler/Common.hs
@@ -20,3 +20,7 @@ getFaviconR = do cacheSeconds $ 60 * 60 * 24 * 30 -- cache for a month
 getRobotsR :: Handler TypedContent
 getRobotsR = return $ TypedContent typePlain
                     $ toContent $(embedFile "config/robots.txt")
+
+getAdsR :: Handler TypedContent
+getAdsR = return $ TypedContent typePlain
+                    $ toContent $(embedFile "config/ads.txt")
