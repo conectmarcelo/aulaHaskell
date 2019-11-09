ca86e42b792195564e3f94583a7af5de1d76b705
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-09 14:27:49 +0000

    aws

--- src/Handler/Orcamentos.hs
+++ src/Handler/Orcamentos.hs
@@ -24,40 +24,7 @@ getOrcamentosR :: Handler Html
 getOrcamentosR = do 
     (widget,enctype) <- generateFormPost formOrcamentos 
     defaultLayout $ do 
-        msg <- getMessage
-        [whamlet|
-            
-            <nav class="navbar justify-content-end navbar-fixed-top">    
-              <ul>
-                <nav class="navbar justify-content-end fixed-top">    
-                    <ul>
-                        <li>
-                            <a href=@{HomeR}>Smiles Festas
-                        <li>
-                            <a href=@{FotosR}>Fotos
-                        <li>
-                            <a href=@{PromocoesR}>Promoções
-                        <li>
-                            <a href=@{ServicosR}>Serviços
-                        <li>
-                            <a href=@{DicasR}>Dicas
-                        <li>
-                            <a href=@{ContatosR}>Contatos    
-
-            $maybe mensa <- msg
-                <div>
-                    ^{mensa}
-            
-            <h1>
-                Solicite um orçamento
-                
-            <form method=post action=@{OrcamentosR}>
-                ^{widget}
-                <input type="submit" value="Cadastrar">
-        |]
-        
-        
-         toWidgetHead [lucius|
+        toWidgetHead [lucius|
             
             * {
             margin:0px;
@@ -143,6 +110,40 @@ getOrcamentosR = do
              
             
         |]
+        msg <- getMessage
+        [whamlet|
+            
+            <nav class="navbar justify-content-end navbar-fixed-top">    
+              <ul>
+                <nav class="navbar justify-content-end fixed-top">    
+                    <ul>
+                        <li>
+                            <a href=@{HomeR}>Smiles Festas
+                        <li>
+                            <a href=@{FotosR}>Fotos
+                        <li>
+                            <a href=@{PromocoesR}>Promoções
+                        <li>
+                            <a href=@{ServicosR}>Serviços
+                        <li>
+                            <a href=@{DicasR}>Dicas
+                        <li>
+                            <a href=@{ContatosR}>Contatos    
+
+            $maybe mensa <- msg
+                <div>
+                    ^{mensa}
+            
+            <h1>
+                Solicite um orçamento
+                
+            <form method=post action=@{OrcamentosR}>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
+        |]
+        
+        
+         
 
 postOrcamentosR :: Handler Html
 postOrcamentosR = do 
