b144dc3d9c80e5beeb4613ff04bbd5d79f590e98
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-09 15:13:02 +0000

    aws

--- src/Handler/Orcamentos.hs
+++ src/Handler/Orcamentos.hs
@@ -94,33 +94,32 @@ getOrcamentosR = do
         [whamlet|
             
             
-                <nav class="navbar justify-content-end fixed-top">    
-                    <ul>
-                        <li>
-                            <a href=@{HomeR}>Smiles Festas
-                        <li>
-                            <a href=@{FotosR}>Fotos
-                        <li>
-                            <a href=@{PromocoesR}>Promoções
-                        <li>
-                            <a href=@{ServicosR}>Serviços
-                        <li>
-                            <a href=@{DicasR}>Dicas
-                        <li>
-                            <a href=@{ContatosR}>Contatos    
-           
-                $maybe mensa <- msg
-                    <div>
-                        ^{mensa}
+        <nav class="navbar justify-content-end fixed-top">    
+            <ul>
+                <li>
+                    <a href=@{HomeR}>Smiles Festas
+                <li>
+                    <a href=@{FotosR}>Fotos
+                <li>
+                    <a href=@{PromocoesR}>Promoções
+                <li>
+                    <a href=@{ServicosR}>Serviços
+                <li>
+                    <a href=@{DicasR}>Dicas
+                <li>
+                    <a href=@{ContatosR}>Contatos    
+   
+        $maybe mensa <- msg
+            <div>
+                ^{mensa}
                 
-                <h1>
-                    Solicite um orçamento
+        <h1>
+            Solicite um orçamento
                 
                 
-                <form method=post action=@{OrcamentosR}>
-                    ^{widget}
-                    <input class ="btn btn-lg button" a where
-                        type="submit" value="Cadastrar">
+        <form method=post action=@{OrcamentosR}>
+            ^{widget}
+            <input class="btn btn-lg" type="submit" value="Cadastrar">
                 
         |]
         
