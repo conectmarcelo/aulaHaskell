86d5478c1376787cb747f45a5aa4bae40dac903b
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-13 23:51:44 +0000

    login

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -12,53 +12,49 @@ import Text.Julius
 --import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
 
-formUsu :: Form (Usuario< Text)
-formUsu = rederBootstrap $ (,)
--- ou  formUsu = rederDivs
+formUsu :: Form (Usuario, Text)
+formUsu = renderBootstrap $ (,)
     <$> (Usuario 
         <$> areq textField "Nome: " Nothing
         <*> areq emailField "E-mail: " Nothing
         <*> areq passwordField "Senha: " Nothing)
     <*> areq passwordField "Digite Novamente: " Nothing
 
-
 getUsuarioR :: Handler Html
-getUsuarioR = do
+getUsuarioR = do 
     (widget,_) <- generateFormPost formUsu
     msg <- getMessage
-    defaulLayout $
+    defaultLayout $ 
         [whamlet|
-            $maybe mensa <- msg
+            $maybe mensa <- msg 
                 <div>
                     ^{mensa}
+            
             <h1>
                 CADASTRO DE USUARIO
-                
+            
             <form method=post action=@{UsuarioR}>
                 ^{widget}
                 <input type="submit" value="Cadastrar">
-        
         |]
 
 postUsuarioR :: Handler Html
-postUsuarioR = do
-    ((result,_),) <- runFormPost formUsu
-    case result of
-        FormSuccess (usuario,veri) -> do
-            if (usuarioSenha usuario == veri) then do
-                runDB $ insert usuario
+postUsuarioR = do 
+    ((result,_),_) <- runFormPost formUsu
+    case result of 
+        FormSuccess (usuario,veri) -> do 
+            if (usuarioSenha usuario == veri) then do 
+                runDB $ insert usuario 
                 setMessage [shamlet|
                     <div>
                         USUARIO INCLUIDO
-                    |]
-                    
-                    redirect UsuarioR
-                else do
-                    setMessage[shamlet|
-                        <div>
-                            SENHA E VERIFICACAO NAO CONFEREM
-                    
-                    |]
-                    redirec UsuarioR
-            -> redirect HomeR
+                |]
+                redirect UsuarioR
+            else do 
+                setMessage [shamlet|
+                    <div>
+                        SENHA E VERIFICACAO N CONFEREM
+                |]
+                redirect UsuarioR
+        _ -> redirect HomeR
 
