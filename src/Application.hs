835bead8bcda71fd83adae0e30e0f5fafc39dfdc
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-14 22:46:56 +0000

    login 2

--- src/Application.hs
+++ src/Application.hs
@@ -22,6 +22,7 @@ import Database.Persist.Postgresql          (createPostgresqlPool, pgConnStr,
                                              pgPoolSize, runSqlPool)
 import Import
 import Language.Haskell.TH.Syntax           (qLocation)
+import Network.Wai.Handler.WarpTLS
 import Network.Wai (Middleware)
 import Network.Wai.Handler.Warp             (Settings, defaultSettings,
                                              defaultShouldDisplayException,
@@ -142,24 +143,34 @@ develMain = develMainHelper getApplicationDev
 -- | The @main@ function for an executable running this site.
 appMain :: IO ()
 appMain = do
+    let cp s = "/etc/letsencrypt/live/smilesfestas.ml/" ++ s
+    settings <- loadYamlSettingsArgs
+        [configSettingsYmlValue]
+        useEnv
+    foundation <- makeFoundation settings
+    app <- makeApplication foundation
+    runTLS
+        (tlsSettingsChain (cp "cert.pem") [cp "chain.pem"] (cp "privkey.pem"))
+        (warpSettings foundation)
+        app
     
 
     -- Get the settings from all relevant sources
-    settings <- loadYamlSettingsArgs
+    --settings <- loadYamlSettingsArgs
         -- fall back to compile-time values, set to [] to require values at runtime
-        [configSettingsYmlValue]
+        --[configSettingsYmlValue]
 
         -- allow environment variables to override
-        useEnv
+        --useEnv
 
     -- Generate the foundation from the settings
-    foundation <- makeFoundation settings
+    --foundation <- makeFoundation settings
 
     -- Generate a WAI Application from the foundation
-    app <- makeApplication foundation
+    --app <- makeApplication foundation
 
     -- Run the application with Warp
-    runSettings (warpSettings foundation) app
+    --runSettings (warpSettings foundation) app
 
 
 --------------------------------------------------------------
