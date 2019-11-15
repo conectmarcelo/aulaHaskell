9708d434c02578c91d77d20aa8ceb8ac70ff6634
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-15 15:55:52 +0000

    login 2

--- src/Handler/Usuario.hs
+++ src/Handler/Usuario.hs
@@ -24,120 +24,7 @@ formUsu = renderBootstrap $ (,)
 getUsuarioR :: Handler Html
 getUsuarioR = do 
     (widget,_) <- generateFormPost formUsu
-    defaultLayout $ do
-        toWidgetHead [lucius|
-            
-        * {
-        margin:0px;
-        padding: 0px;
-        }
-        
-        nav{
-        background-color:rgba(0,0,0, 0.9)
-        }
-
-        ul{  
-            padding: 20px;
-            text-align: end;
-        }
-        
-        li {
-            list-style: none;
-            display: inline;
-            margin-left: 20px;
-        }
-        
-        li a{
-            text-decoration: none;
-            color:white;
-            font-family: Helvetica;
-            font-size: 20px;
-            padding: 20px;
-            transition-duration:0.5s, 0.3s;
-            cursor: pointer;
-        }
-        
-        li a:hover{
-            width: 210px;
-            background-color: grey;
-            color:white;
-            font-size: 20px;
-            padding: 25px;
-            text-decoration: none;
-        }
-        
-        .mapa {
-            background-image: url(@{StaticR mapa_jpg});
-            background-repeat: no-repeat;
-            background-size: 100%;
-            
-            
-            
-        }
-         
-         .contato {
-                    background-color:lightgrey;
-                    Text-align: center;
-                    margin-top:50px;
-                    }
-        .button{
-                background-color:black;
-                color:white
-        }
-        
-        h1{color:rgb(192, 159, 75);
-        }
-        
-        .form{background-color:#e9e9e9;
-        padding:0px;       
-        
-            
-        }            
-     
-        input[type=text], select {
-          width: 100%;
-          padding: 12px 20px;
-          margin: 8px 0;
-          display: inline-block;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-          box-sizing: border-box;
-        }
-        
-        input[type=submit] {
-          width: 100%;
-          background-color: black;
-          color: white;
-          padding: 14px 20px;
-          margin: 8px 0;
-          border: none;
-          border-radius: 4px;
-          cursor: pointer;
-        }
-        
-        input[type=submit]:hover {
-          background-color: grey;
-        }
-        
-        div {
-          border-radius: 5px;
-        
-        }
-        
-        .form {padding:100px 200px 50px 200px;
-                margin:50px;
-        }
-        
-        footer{background-color:black;
-            text-align:center;
-            padding:20px;}
-        img {width:50px; height:50px;}
-        
-        .rodape{text-align:center}
-        
-                    
-        |]
-        
+    defaultLayout $
         msg <- getMessage
         [whamlet|
             
@@ -159,9 +46,7 @@ getUsuarioR = do
                     <a href=@{EntrarR}Login
                 <li>
                     <a href=@{ListOrcamentosR}>OrcamentosR
-                <li>
-            
-            
+               
             $maybe mensa <- msg 
 
                 <div>
