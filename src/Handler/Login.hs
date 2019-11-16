07f494aefa625a7e39cfcb2728a979ec31362564
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-16 00:22:04 +0000

    login 2

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -211,7 +211,73 @@ postSairR = do
 
 getAdminR :: Handler Html
 getAdminR = do 
-    defaultLayout [whamlet|
-        <h1>
-            BEM-VINDO MEU REI!
-    |]
+    defaultLayout $ do
+        toWidgetHead [lucius|
+            
+        * {
+        margin:0px;
+        padding: 0px;
+        }
+        
+        nav{
+        background-color:rgba(0,0,0, 0.9)
+        }
+
+        ul{  
+            padding: 20px;
+            text-align: end;
+        }
+        
+        li {
+            list-style: none;
+            display: inline;
+            margin-left: 20px;
+        }
+        
+        li a{
+            text-decoration: none;
+            color:white;
+            font-family: Helvetica;
+            font-size: 20px;
+            padding: 20px;
+            transition-duration:0.5s, 0.3s;
+            cursor: pointer;
+        }
+        
+        li a:hover{
+            width: 210px;
+            background-color: grey;
+            color:white;
+            font-size: 20px;
+            padding: 25px;
+            text-decoration: none;
+        }
+        
+                    
+        |]
+        
+        [whamlet|
+        <nav class="navbar justify-content-end fixed-top">    
+            <ul>
+                <li>
+                    <a href=@{HomeR}>Home
+                <li>
+                    <a href=@{FotosR}>Fotos
+                <li>
+                    <a href=@{PromocoesR}>Promoções
+                <li>
+                    <a href=@{ServicosR}>Serviços
+                <li>
+                    <a href=@{DicasR}>Blog
+                <li>
+                    <a href=@{ContatosR}>Contatos
+                <li>
+                    <a href=@{UsuarioR}>Cadastrar
+                <li>
+                    <a href=@{EntrarR}>Login
+                
+
+        <div class="container">
+            <h1>
+                BEM-VINDO A ÁREA ADMINISTRATIVA!
+        |]
