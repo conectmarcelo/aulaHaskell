be82c2d819d81edf9222317a40557933839a9cf1
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-17 20:53:22 +0000

    banner

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -81,9 +81,7 @@ getHomeR = do
         addScriptRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
         sess <- lookupSession "_NOME"
         toWidgetHead [hamlet|
-        addStylesheet (StaticR css_bannertxt_css)
-        addScript (StaticR css_bannertxt_js)
-        <title>Smiles Festas
+        
         <script data-ad-client="ca-pub-6395641199023717" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
         
         
