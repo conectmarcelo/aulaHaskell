a59c72b58336ce8a984fceca71c36543f0c48c8e
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-18 23:47:01 +0000

    margin main

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -222,7 +222,7 @@ getHomeR = do
         
         <div class="a">
             <div class="aa">
-                <h1>SMILES FESTAS.
+                <h1>SMILES FESTAS
                 <h2>Casamentos, Debutantes, Aniversários e muito mais    
                 
         <div class="b">
