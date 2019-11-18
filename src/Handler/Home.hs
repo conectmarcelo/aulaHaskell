615240ff947e32762948c301eafd973939f09fcf
Ubuntu <ubuntu@ip-172-31-89-183.ec2.internal>
2019-11-18 22:36:12 +0000

    margin main

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -96,8 +96,6 @@ getHomeR = do
         <link rel="stylesheet" type="text/css" href="bannertxt.css" media="screen" />
         <script type="text/javascript" src="bannertxt.js">
         
-        <script data-ad-client="ca-pub-6395641199023717" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">
-        
         
         |]
         
