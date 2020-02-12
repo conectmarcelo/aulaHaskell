ac8c6fb56fc167d836a539f34818c21e2783d30e
EC2 Default User <ec2-user@ip-172-31-46-140.us-east-2.compute.internal>
2020-02-12 18:40:35 +0000

    80

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 git add . &&
 git commit -m "$1" &&
 git push origin master &&
-ssh root@51.91.127.28 <<EOF
+ssh root@51.77.192.38 <<EOF
 cd aulaHaskell &&
 stack clean &&
 git pull origin master &&
 stack build &&
-lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
+lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
 nohup stack exec aulahaskell > /dev/null 
 echo "deploy finished"
 EOF
+
