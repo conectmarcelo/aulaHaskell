20da74c3602233d2f32b16a6ca4fadee670ac5f7
EC2 Default User <ec2-user@ip-172-31-46-140.us-east-2.compute.internal>
2020-02-20 21:24:17 +0000

    tls

--- src/Application.hs
+++ src/Application.hs
@@ -143,7 +143,7 @@ develMain = develMainHelper getApplicationDev
 -- | The @main@ function for an executable running this site.
 appMain :: IO ()
 appMain = do
-   let cp s = "/etc/letsencrypt/live/smilesfestas.ml/" ++ s
+    let cp s = "/etc/letsencrypt/live/smilesfestas.ml/" ++ s
     settings <- loadYamlSettingsArgs
         [configSettingsYmlValue]
         useEnv
