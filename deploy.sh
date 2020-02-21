3b7e4a5c8558a188ced982d0d8ee832ae2a3f700
EC2 Default User <ec2-user@ip-172-31-46-140.us-east-2.compute.internal>
2020-02-21 05:19:54 +0000

    tls

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ cd aulaHaskell &&
 stack clean &&
 git pull origin master &&
 stack build &&
-lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
+lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
 nohup stack exec aulahaskell > /dev/null 
 echo "deploy finished"
 EOF
